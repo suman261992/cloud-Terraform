f05a4e0fd4171c96c173b766bbc7373f428babd4
suman261992 <sumanp1992.12.26@gmail.com>
2025-02-08 12:04:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,11 +14,10 @@ terraform {
     } 
   } 
 }
-
 provider "aws" {
   region="us-east-1"
 }
 resource "aws_vpc" "main1" {
   cidr_block = "192.168.2.0/24"
- tags = {Name = "dev-vcp"}
+ tags = {Name = "main1"}
 }
